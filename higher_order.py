4a17799bb3e05e0c3086833d7e885793040365f5
ad3rinto <aad3rinto@gmail.com>
2023-10-27 17:03:00 +0100

    solved paddle challenge

--- higher_order.py
+++ higher_order.py
@@ -1,13 +1,40 @@
+import turtle
 from turtle import Turtle, Screen
 
 tade = Turtle()
 screen = Screen()
+tade.hideturtle()
 
 
-def move_forward():
+def move_right():
+    tade.setheading(0)
     tade.forward(10)
 
 
+def move_left():
+    tade.setheading(180)
+    tade.forward(10)
+
+
+def move_upwards():
+    tade.setheading(90)
+    tade.forward(10)
+
+
+def move_downwards():
+    tade.setheading(270)
+    tade.forward(10)
+
+
+def clear_drawing():
+    tade.clear()
+
+
 screen.listen()
-screen.onkey(move_forward, "a")
+screen.onkey(move_right, "k")
+screen.onkey(move_left, "h")
+screen.onkey(clear_drawing, "c")
+screen.onkey(move_upwards, "u")
+screen.onkey(move_downwards, "n")
+
 screen.exitonclick()
